dbe1487b11cb52fdc1ceaff2b01f6f3b89faeaaa
DANNIEL <donghoon149@gmail.com>
2020-03-12 04:10:26 +0900

    feat:auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from "react";
-
-import Firebase from "firebase";
-import config from "./config";
-import Test from "./pages/test";
+import React, { Component } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./config";
+import logo from "./logo.svg";
+import "./App.css";
 import Home from "./pages/home";
-import Join from "./components/join";
-import Login from "./components/login";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    Firebase.initializeApp(config.firebase);
-  }
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+class App extends Component {
   render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+
     return (
-      <div>
-        <Login />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+
+          {user ? (
+            <button onClick={signOut}>Sign out</button>
+          ) : (
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+          )}
+          <Home />
+        </header>
       </div>
     );
   }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);
+
+// import React from "react";
+
+// import Firebase from "firebase";
+// import "firebase/auth";
+// import config from "./config";
+// import Test from "./pages/test";
+// import Home from "./pages/home";
+// import Join from "./components/join";
+// import Login from "./components/login";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     Firebase.initializeApp(config.firebase);
+//     this.auth = Firebase.auth();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Login />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
